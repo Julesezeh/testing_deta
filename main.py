aeef317a26163fbcf6e42b56b796c6a39e742815
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-09-07 08:42:24 +0100

    initialization and setting dependency

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
-
+from .database import sessionLocal, engine
 
 app = FastAPI()
+
+
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
