eb4b3fa18bfd32437d61303a2fca66921372c480
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-09-08 02:32:10 +0100

    also tweak to teacher model

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import String, Integer, Column
 from .database import Base
 
 
-class Teachers(Base):
+class Teacher(Base):
+    __tablename__ = "teacher"
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True)
